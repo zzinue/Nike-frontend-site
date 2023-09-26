35876476312ce3305608b09b50267845215e5fad
zzinue <zzinue@gmail.com>
2023-09-26 14:09:37 -0600

    adding superquality , servicecard and popularproducts

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@
 const Button = ({label, iconUrl}) => {
   return (
     <button className='flex justify-center items-center gap-3 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>{label}
-    <img src={iconUrl} alt="arrow right" className="ml-2 rounded-full w-5 h-5" /></button>
+    {iconUrl&& <img src={iconUrl} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5" />}</button>
   )
 }
 
--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,15 +1,17 @@
 const ShoeCard = ({imgURL,changeBigShoeImage,bigShoeImg}) => {
 const handleClick=() => {
-if(bigShoeImg !== imgURL.tumbnail){
-    changeBigShoeImage(imgURL.tumbnail)
+if(bigShoeImg !== imgURL.bigShoe){
+    changeBigShoeImage(imgURL.bigShoe)
 }
 }
   return (
 <div className={`border-2 rounded-xl
-${bigShoeImg===imgURL
+${bigShoeImg===imgURL.bigShoe
 ? 'border-coral-red'
-: 'border-transparent'} cursor-pointer max-sm:flex-1` } onClick={handleClick}>
-    <div>
+: 'border-transparent'}
+ cursor-pointer max-sm:flex-1` } 
+ onClick={handleClick}>
+    <div className="flex justify-center items- bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
         <img src={imgURL.bigShoe}
         alt="shoe collection"
         width={127}
