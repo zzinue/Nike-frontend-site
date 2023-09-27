18da08794c9478b197044ff6ec6b50aecf616c92
zzinue <zzinue@gmail.com>
2023-09-26 18:41:28 -0600

    product quality and super offer built

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,16 @@
 
 
-const Button = ({label, iconUrl}) => {
+const Button = ({label, iconUrl, backgroundColor, textColor, borderColor}) => {
   return (
-    <button className='flex justify-center items-center gap-3 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>{label}
+    <button className={`flex justify-center items-center gap-3 px-7 py-4 border font-montserrat text-lg leading-none
+    ${
+      backgroundColor
+    ? `${backgroundColor} $
+    {textColor} ${borderColor}`
+    :"bg-coral-red text-white  border-coral-red"} rounded-full w-full} `}
+    >{label}
     {iconUrl&& <img src={iconUrl} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5" />}</button>
-  )
+  ) 
 }
 
 export default Button
--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,7 +1,12 @@
 
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+    <section className="max-container">
+      <h3 className="font-palaquin text-center text-4xl font-bold ">What our?
+        <span className="text-coral-red">Customers</span>
+      Say?</h3>
+<p className=" info-text m-auto  mt-4 max-w-lg text-center">Hear genuine stories from our satisfied customers about their exceptional experiences with us.</p>
+    </section>
   )
 }
 
